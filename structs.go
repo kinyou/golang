bd9d73267e76f95327b3587c6ffb988b1008ed2a
wangxingyuan <nova_wangxy@126.com>
2018-08-10 18:39:31 +0800

    go的结构体

--- structs.go
+++ structs.go
@@ -10,6 +10,27 @@ type person struct {
 }
 
 func main(){
+	//创建新的结构体
 	fmt.Println(person{"Bob",20})
+
+	fmt.Println(person{name:"Alice",age:30})
+
+	//省略的字段被初始化为零值
+	fmt.Println(person{name:"Fred"})
+
+	//&前缀生成一个结构体指针
+	fmt.Println(&person{name:"Ann",age:40})
+
+	s := person{name :"	Sean",age:50}
+	//使用点来访问结构体的字段
+	fmt.Println(s.name)
+
+	//也可以对结构体指针使用.-指针会被自动解引用
+	sp := &s
+	fmt.Println(sp.age)
+
+	//结构体是可变的
+	sp.age = 51
+	fmt.Println(sp.age)
 
 }
